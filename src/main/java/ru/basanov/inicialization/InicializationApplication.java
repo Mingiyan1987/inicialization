198c7f993365fab87690800faac4025d5f27c526
Mingiyan1987 <basanov.mt87@gmail.com>
2019-03-03 10:11:31 +0300

    3

---
+++ src/main/java/ru/basanov/inicialization/InicializationApplication.java
@@ -0,0 +1,13 @@
+package ru.basanov.inicialization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InicializationApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(InicializationApplication.class, args);
+	}
+
+}
